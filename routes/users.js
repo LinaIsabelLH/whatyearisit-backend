207c45e715ea8a6305df60bd68daf53a344e05ba
Lina <londonohidalgo@gmail.com>
2024-09-20 11:25:22 +0200

    backend

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  res.json({year: '2024'});
+});
+
+module.exports = router;
